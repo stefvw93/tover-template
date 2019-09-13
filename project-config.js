2f28acadb76dab61a8de642922b676f235a4f8a0
Stef <stefvanwijchen@gmail.com>
2019-09-13 15:04:24 +0200

    structure alphabetically

--- project-config.js
+++ project-config.js
@@ -5,19 +5,19 @@ module.exports = {
     title: "React boilerplate"
   },
 
-  entry: "./index.js",
-
-  paths: {
-    compiled: path.join(__dirname, "__compiled"),
-    distribution: path.join(__dirname, "_dist"),
-    webpack: path.join(__dirname, "_webpack"),
-    HTMLTemplate: path.join(__dirname, "src", "index.template.pug")
-  },
-
   devServer: {
     hot: true,
     ip: "localhost",
     open: true,
     port: 12020
+  },
+
+  entry: "./index.js",
+
+  paths: {
+    compiled: path.join(__dirname, "__compiled"),
+    distribution: path.join(__dirname, "_dist"),
+    HTMLTemplate: path.join(__dirname, "src", "index.template.pug"),
+    webpack: path.join(__dirname, "_webpack")
   }
 };
