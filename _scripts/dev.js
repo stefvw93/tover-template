3bef893231d655610283ff65916444b879dfea71
Stef <stefvanwijchen@gmail.com>
2019-09-20 10:59:57 +0200

    remove kill port package

--- _scripts/dev.js
+++ _scripts/dev.js
@@ -1,12 +1,9 @@
 const { spawn } = require("child_process");
 const { paths, devServer } = require("../project-config");
-const killPort = require("kill-port");
 const colors = require("colors/safe");
 
 // show api url
 console.log(`${colors.green.bold("Starting development mode...")}`);
-
-killPort(devServer.port);
 
 const tscTask = spawn("tsc", ["-w"], { stdio: "inherit" });
 const webpackTask = spawn(
