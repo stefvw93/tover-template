7809937341ac669477084cfdcdda264233e36737
Stef van Wijchen <stefvanwijchen@gmail.com>
2019-11-23 12:12:59 +0100

    Update React version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,33 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
-  extends: ["plugin:prettier/recommended", "prettier/react"],
-  parser: "@typescript-eslint/parser",
+  extends: ['plugin:prettier/recommended', 'prettier/react'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module',
   },
-  plugins: ["react", "prettier", "@typescript-eslint"],
+  plugins: ['react', 'prettier', '@typescript-eslint'],
   rules: {
-    "prettier/prettier": "warn",
-    "@typescript-eslint/explicit-member-accessibility": "error",
+    'prettier/prettier': 'warn',
+    '@typescript-eslint/explicit-member-accessibility': 'error',
   },
   settings: {
     react: {
-      version: "16.10"
-    }
-  },overrides: [
+      version: '16.12',
+    },
+  },
+  overrides: [
     {
       files: ['*.js'],
       rules: {
-        "@typescript-eslint/explicit-member-accessibility": "off",
+        '@typescript-eslint/explicit-member-accessibility': 'off',
       },
-    }
-  ]
+    },
+  ],
 };
