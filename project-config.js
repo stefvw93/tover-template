859b289124ec20860b3a4b574a84653ad69d12c7
Stef <stefvanwijchen@gmail.com>
2019-12-10 10:40:45 +0100

    User current working dir instead of __dirname

--- project-config.js
+++ project-config.js
@@ -22,11 +22,11 @@ module.exports = {
 
   paths: {
     dirnames,
-    root: __dirname,
-    compiled: path.join(__dirname, dirnames.compiled),
-    distribution: path.join(__dirname, dirnames.distribution),
-    source: path.join(__dirname, dirnames.source),
-    htmlTemplate: path.join(__dirname, dirnames.source, 'index.html'),
-    webpack: path.join(__dirname, dirnames.webpack),
+    root: process.cwd(),
+    compiled: path.join(process.cwd(), dirnames.compiled),
+    distribution: path.join(process.cwd(), dirnames.distribution),
+    source: path.join(process.cwd(), dirnames.source),
+    htmlTemplate: path.join(process.cwd(), dirnames.source, 'index.html'),
+    webpack: path.join(process.cwd(), dirnames.webpack),
   },
 };
