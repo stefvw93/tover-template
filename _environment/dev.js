e576ef7b31eea3bdf1615abf1af7fc6387a4acef
Stef <stefvanwijchen@gmail.com>
2019-12-11 18:35:17 +0100

    Add cwd

--- _environment/dev.js
+++ _environment/dev.js
@@ -4,9 +4,12 @@ const colors = require('colors/safe');
 const childProcessOptions = {
   stdio: 'inherit',
   shell: /^win/.test(process.platform),
+  cwd: process.cwd(),
 };
 
-console.log(`${colors.green.bold('Starting development mode...')}`);
+console.log(
+  `${colors.green.bold('Starting development mode...')} @ ${process.cwd()}`
+);
 
 spawnSync('tsc', ['--incremental']);
 spawn('tsc', ['-w', '--incremental'], childProcessOptions);
